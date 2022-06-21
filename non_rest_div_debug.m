915305644a7949f0671eb6d3772d037825740ce8
Rishi-T <36669914+Rishi-T@users.noreply.github.com>
2022-06-21 13:19:03 +0530

    Add files via upload

---
+++ non_rest_div_debug.m
@@ -0,0 +1,60 @@
+x = input("Enter Dividend value : ");
+y = input("Enter Divisor value : ");
+
+n=log2(x);
+			if x==0
+				n=1;
+			elseif n==floor(n)
+				n=n+1;
+			else
+				n=ceil(n);
+			end%if
+
+Q = fi(x,0,n,0,'OverflowAction','Wrap');
+M = fi(y,0,n,0);
+A = fi(0,0,8,0,'OverflowAction','Wrap');
+
+fprintf('n  M ')
+for c = 1:n
+    fprintf(' ')
+end
+fprintf('A         Q\n')
+
+for count = 1:n
+
+    fprintf('%d  %s  %s  %s  cycle start\n',n-count+1,dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+
+   if getmsb(A) == 1
+    A = bitshift(A,1);
+
+    if getmsb(Q) == 1
+    A = bitset(A,1);
+    end
+    A = accumpos(A,M);
+    Q = bitshift(Q,1);
+    fprintf('   %s  %s  %s  shift left AQ & A = A + M (LSB of Q is not finalised)\n',dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+   else
+     A = bitshift(A,1);
+
+    if getmsb(Q) == 1
+     A = bitset(A,1);
+    end
+     Q = bitshift(Q,1);
+     A = accumneg(A,M);
+     fprintf('   %s  %s  %s  shift left AQ & A = A - M (LSB of Q is not finalised)\n',dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+   end
+
+   if getmsb(A) == 0
+       Q = bitset(Q,1);
+       fprintf('   %s  %s  %s  Q[0] = 1\n',dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+   else
+       fprintf('   %s  %s  %s  Q[0] = 0\n',dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+   end
+end
+
+if getmsb(A) == 1
+    A = accumpos(A,M);
+    fprintf('   %s  %s  %s  A = A + M\n',dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+end
+
+fprintf('Quotient = %d\nRemainder = %d\n',Q,A);
---
+++ rest_div_debug.m
@@ -0,0 +1,52 @@
+x = input("Enter Dividend value : ");
+y = input("Enter Divisor value : ");
+
+n=log2(x);
+			if x==0
+				n=1;
+			elseif n==floor(n)
+				n=n+1;
+			else
+				n=ceil(n);
+			end%if
+
+Q = fi(x,0,n,0,'OverflowAction','Wrap');
+M = fi(y,0,n,0);
+A = fi(0,0,8,0,'OverflowAction','Wrap');
+
+fprintf('n  M ')
+for c = 1:n
+    fprintf(' ')
+end
+fprintf('A         Q\n')
+%for c = 1:n
+%    fprintf(' ')
+%end
+
+for count = 1:n
+   fprintf('%d  %s  %s  %s  cycle start\n',n-count+1,dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+   
+   A = bitshift(A,1);
+
+   if getmsb(Q) == 1
+    A = bitset(A,1);
+   end
+
+   Q = bitshift(Q,1);
+
+   fprintf('   %s  %s  %s  shift left AQ (LSB of Q is not finalised)\n',dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+
+   A = accumneg(A,M);
+
+   fprintf('   %s  %s  %s  A = A - M\n',dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+
+   if getmsb(A)==0
+       Q = bitset(Q,1);
+       fprintf('   %s  %s  %s  Q[0] = 1\n',dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+   else
+       A = accumpos(A,M);
+       fprintf('   %s  %s  %s  Q[0] = 0 & Restore A\n',dec2bin(M,n),dec2bin(A,8),dec2bin(Q,n))
+   end
+end
+
+fprintf('Quotient = %d\nRemainder = %d\n',Q,A);
